633ec093ba8e0a3b1a0a7a901837b51e44b45d58
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-04-13 12:14:23 +0300

    1 task

--- AlgorithmsAndDataStruct/Program.cs
+++ AlgorithmsAndDataStruct/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace AlgorithmsAndDataStruct
+{
+    static class AlgorithmsAndDataStruct
+    {
+        public static void Main()
+        {
+            IsNumberPrime(7); // простое
+            IsNumberPrime(1); // простое
+            IsNumberPrime(11); // простое 
+            IsNumberPrime(6); // не простое 
+            IsNumberPrime(80); // не простое
+        }
+
+        static void IsNumberPrime(int number)
+        {
+            int d = 0, i = 2;
+
+            while (i < number)
+            {
+                if (number % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else
+                    i++;
+            }
+
+            Console.WriteLine(d == 0 ? "Простое." : "Не простое.");
+        }
+    }
+}
